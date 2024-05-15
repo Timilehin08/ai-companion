def4e80bac82a55a59f812c842c2f5550809beba
Timilehin08 <timilehindebra@gmail.com>
2024-05-15 01:26:05 +0100

    sidebar created on sm and lg screens

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,6 +9,8 @@ import { UserButton } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "./mobile-sidebar";
+
 
 const font = Poppins ({
     weight: "600",
@@ -17,9 +19,9 @@ const font = Poppins ({
 
 export const Navbar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
-            <Menu className="block md:hidden"/>
+               <MobileSidebar />
             <Link href="/">
                 <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
                     font.className,
